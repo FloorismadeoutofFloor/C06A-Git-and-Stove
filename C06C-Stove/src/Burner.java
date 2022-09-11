02a0a1500467db6a98eaef8359fc17afc0d69bfe
FloorismadeoutofFloor <gwdina@gmail.com>
2022-09-11 16:16:52 -0600

    added Burner class with Enum variables

---
+++ C06C-Stove/src/Burner.java
@@ -0,0 +1,13 @@
+
+public class Burner {
+	enum Temperature{BLAZING, HOT, WARM, COLD}
+	Temperature myTemperature;
+	Setting mySetting;
+	int timer;
+	public final static int TIME_DURATION = 2;
+
+	public Burner() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
