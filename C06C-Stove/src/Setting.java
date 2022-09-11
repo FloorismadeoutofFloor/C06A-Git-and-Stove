678c71151ca229de9f640bfd3a7dff2e4444f849
BenApplegate <applegateben03@gmail.com>
2022-09-11 16:14:23 -0600

    Added Setting enum

---
+++ C06C-Stove/src/Setting.java
@@ -0,0 +1,15 @@
+
+public enum Setting {
+	OFF("---"), LOW("--+"), MEDIUM("-++"), HIGH("+++");
+	
+	private String setting;
+	Setting(String setting){
+		this.setting = setting;
+	}
+	
+	public String toString() {
+		return setting;
+	}
+	
+	
+}
