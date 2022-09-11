96d4132d90564046aa0c05af9f7b6b0409655532
FloorismadeoutofFloor <gwdina@gmail.com>
2022-09-11 17:26:32 -0600

    updated update temp class

--- C06C-Stove/src/Burner.java
+++ C06C-Stove/src/Burner.java
@@ -5,6 +5,7 @@ public class Burner {
 	private Setting mySetting;
 	int timer;
 	public final static int TIME_DURATION = 2;
+	
 
 	public Temperature getMyTemperature() {
 		return myTemperature;
@@ -14,7 +15,7 @@ public class Burner {
 	 * @param myTemperature
 	 * @param mySetting
 	 */
-	public Burner(Burner.Temperature myTemperature, Setting mySetting) {
+	public Burner() {
 		myTemperature = Temperature.COLD;
 		mySetting = Setting.OFF;
 	}
@@ -24,7 +25,7 @@ public class Burner {
 		case OFF: mySetting = Setting.LOW;
 		case LOW: mySetting = Setting.MEDIUM;
 		case MEDIUM: mySetting = Setting.HIGH;
-		case HIGH: System.out.println("At hightest setting");
+		//case HIGH: System.out.println("At hightest setting");
 		}
 		timer = TIME_DURATION;
 	}	
@@ -34,7 +35,7 @@ public class Burner {
 		case HIGH: mySetting = Setting.MEDIUM;
 		case MEDIUM: mySetting = Setting.LOW;
 		case LOW: mySetting = Setting.OFF;
-		case OFF: System.out.println("At Lowest setting");
+		//case OFF: System.out.println("At Lowest setting");
 		}
 		timer = TIME_DURATION;
 	}	
@@ -45,13 +46,38 @@ public class Burner {
 		}
 		else if (timer == 0) {
 			switch(mySetting) {
-			case OFF: myTemperature = Temperature.COLD;
-			case LOW: myTemperature = Temperature.WARM;
-			case MEDIUM: myTemperature = Temperature.HOT;
-			case HIGH: myTemperature = Temperature.BLAZING;
+			case OFF:{
+				switch (myTemperature){ 
+				case WARM: myTemperature = Temperature.COLD;
+				case HOT: myTemperature = Temperature.WARM; timer = TIME_DURATION;
+				case BLAZING: myTemperature = Temperature.HOT; timer = TIME_DURATION;
+				}
+			};
+			case LOW:{ 
+				switch (myTemperature){ 
+				case COLD: myTemperature = Temperature.WARM;
+				case HOT: myTemperature = Temperature.WARM;
+				case BLAZING: myTemperature = Temperature.HOT; timer = TIME_DURATION;
+				}
+			};
+			case MEDIUM: {
+				switch (myTemperature){ 
+				case COLD: myTemperature = Temperature.WARM; timer = TIME_DURATION;
+				case WARM: myTemperature = Temperature.HOT;
+				case BLAZING: myTemperature = Temperature.HOT;
+				}
+			};
+			case HIGH:{
+				switch (myTemperature){ 
+				case COLD: myTemperature = Temperature.WARM; timer = TIME_DURATION;
+				case WARM: myTemperature = Temperature.HOT; timer = TIME_DURATION;
+				case HOT: myTemperature = Temperature.BLAZING;
+				}
+			};
 		}
 		}
 	}
+	
 	
 	public String display() {
 		switch(myTemperature) {
